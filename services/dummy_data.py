4c6d3a8c591fb5720190a449fb77d79452603569
Frank Roth <Frank_Roth@swissre.com>
2022-02-13 18:55:54 +0100

    changes for app

--- services/dummy_data.py
+++ services/dummy_data.py
@@ -1,3 +1,5 @@
+from flask import abort
+
 data = {
     "todos": [
       {
@@ -34,11 +36,16 @@ def get_todos():
 def add_todo(task):
     task["id"] = len(data['todos']) + 1
     data['todos'].append(task)
+    return task, 201
 
 def edit_todo(task):
     for i,todo in enumerate(data["todos"]):
         if todo["id"] == task["id"]:
            data["todos"][i] = task
+           return task, 200
+    abort (
+      404, "Taks with id {id} not found".format(id=task["id"])
+    )
 
 def delete_todo(task_id):
     item_to_remove = '';
@@ -47,3 +54,8 @@ def delete_todo(task_id):
             item_to_remove = todo
     if item_to_remove:
         data["todos"].remove(item_to_remove)
+        return "", 204
+    else:
+      abort (
+        404, "Taks with id {id} not found".format(id=task_id)
+      )

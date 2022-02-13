4ebb12c3f8d51adb3d8d1022392dae6aee5c71e3
Frank Roth <Frank_Roth@swissre.com>
2022-02-13 16:52:16 +0100

    removed retruns that are not used anymore

--- services/dummy_data.py
+++ services/dummy_data.py
@@ -34,20 +34,11 @@ def get_todos():
 def add_todo(task):
     task["id"] = len(data['todos']) + 1
     data['todos'].append(task)
-    return {
-      "message": "task added",
-      "number of tasks": len(data['todos'])
-    }
 
 def edit_todo(task):
     for i,todo in enumerate(data["todos"]):
         if todo["id"] == task["id"]:
            data["todos"][i] = task
-    
-    return {
-      "message": "task edited",
-      "number of tasks": len(data['todos'])
-    }
 
 def delete_todo(task_id):
     item_to_remove = '';
@@ -56,9 +47,3 @@ def delete_todo(task_id):
             item_to_remove = todo
     if item_to_remove:
         data["todos"].remove(item_to_remove)
-    
-    return {
-      "message": "task deleted",
-      "number of tasks": len(data['todos'])
-    }
-    

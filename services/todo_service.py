4ce1835b116c33e6b665d641f07228b761d977bd
Frank Roth <Frank_Roth@swissre.com>
2022-02-13 14:39:39 +0100

    frist release

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,5 +1,10 @@
+from readline import append_history_file
 from services.dummy_data import get_todos, add_todo, edit_todo, delete_todo
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/get-tasks")
 def list_todos():
     return get_todos()
 
--- todo-get/__init__.py
+++ todo-get/__init__.py
@@ -2,10 +2,12 @@ import logging, json
 
 import azure.functions as func
 from services.todo_service import get_todos
+from FlaskApp import app
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     data = get_todos()
-    return func.HttpResponse(json.dumps(data), headers={"content-type": "application/json"})
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+    #return func.HttpResponse(json.dumps(data), headers={"content-type": "application/json"})

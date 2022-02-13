75491da9e593b1e7583eaf433c74a135d6812f38
Frank Roth <Frank_Roth@swissre.com>
2022-02-13 16:49:35 +0100

    added connexion with swagger

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,28 +1,8 @@
-from readline import append_history_file
-from tkinter import W
-from services.dummy_data import get_todos, add_todo, edit_todo, delete_todo
-from flask import Flask, jsonify, request, Response
+import connexion
 
+app = connexion.App(__name__, specification_dir="./")
 
-app = Flask(__name__)
-
-@app.route("/api/tasks", methods=['GET'])
-def list_todos():
-    return jsonify(get_todos())
-    
-@app.route("/api/tasks/<task>", methods=['POST', 'PUT'])
-def add_todos(task):
-    if request.method == 'PUT':
-        edit_todo(task)
-        return Response(status=200)
-    if request.method == 'POST':
-        add_todo(task)
-        return Response(status=201)
-
-@app.route("/api/tasks/<id>", methods=['DELETE'])
-def edit_todos(task_id):
-    delete_todo(task_id)
-    return Response(status=200)
+app.add_api("swagger.yml")
 
 if __name__ == "__main__":
     app.run()
--- todo-api/__init__.py
+++ todo-api/__init__.py
@@ -5,4 +5,4 @@ from services.todo_service import app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+    return func.WsgiMiddleware(app.app.wsgi_app).handle(req, context)

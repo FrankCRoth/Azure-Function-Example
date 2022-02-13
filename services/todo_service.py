2544378794c338c89e7a715d6109b0708a1931cf
Frank Roth <Frank_Roth@swissre.com>
2022-02-13 15:29:32 +0100

    better return codes

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,6 +1,7 @@
 from readline import append_history_file
+from tkinter import W
 from services.dummy_data import get_todos, add_todo, edit_todo, delete_todo
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 
 
 app = Flask(__name__)
@@ -13,14 +14,15 @@ def list_todos():
 def add_todos(task):
     if request.method == 'PUT':
         edit_todo(task)
+        return Response(status=200)
     if request.method == 'POST':
         add_todo(task)
-    return jsonify(get_todos())
+        return Response(status=201)
 
 @app.route("/api/tasks/<id>", methods=['DELETE'])
 def edit_todos(task_id):
     delete_todo(task_id)
-    return jsonify(get_todos())
+    return Response(status=200)
 
 if __name__ == "__main__":
     app.run()
